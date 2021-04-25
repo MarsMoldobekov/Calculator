d981aeedcd69aa0d9644e2cd1517945f53d6b409
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-04-25 23:18:49 +0300

    Add methods and implement them

--- app/src/main/java/com/example/calculator/domain/CalculatorImplementation.java
+++ app/src/main/java/com/example/calculator/domain/CalculatorImplementation.java
@@ -1,7 +1,12 @@
 package com.example.calculator.domain;
 
+import com.example.calculator.enums.BracketButtonTypes;
 import com.example.calculator.enums.DigitButtonTypes;
+import com.example.calculator.enums.IrrationalNumberTypes;
+import com.example.calculator.enums.LogarithmicFunctionTypes;
 import com.example.calculator.enums.OperationButtonTypes;
+import com.example.calculator.enums.RootTypes;
+import com.example.calculator.enums.TrigonometricFunctionTypes;
 
 import net.objecthunter.exp4j.ExpressionBuilder;
 
@@ -44,6 +49,93 @@ public class CalculatorImplementation implements Calculator {
                 break;
             case NINE:
                 stringBuilder.append(9);
+                break;
+        }
+
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public String onBracketButtonsPressed(BracketButtonTypes type) {
+        switch (type) {
+            case CLOSE:
+                stringBuilder.append(')');
+                break;
+            case OPEN:
+                stringBuilder.append('(');
+                break;
+        }
+
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public String onDeleteButtonPressed() {
+        int length = stringBuilder.length();
+
+        if (length > 0) {
+            stringBuilder.deleteCharAt(length - 1);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public String onIrrationalNumberButtonsPressed(IrrationalNumberTypes type) {
+        switch (type) {
+            case PI:
+                stringBuilder.append("pi");
+                break;
+            case E:
+                stringBuilder.append("e");
+                break;
+        }
+
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public String onLogarithmicFunctionButtonsPressed(LogarithmicFunctionTypes type) {
+        switch (type) {
+            case LN:
+                stringBuilder.append("log(");
+                break;
+            case LOG2:
+                stringBuilder.append("log2(");
+                break;
+            case LOG10:
+                stringBuilder.append("log10(");
+                break;
+        }
+
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public String onRootButtonsPressed(RootTypes type) {
+        switch (type) {
+            case SQRT:
+                stringBuilder.append("sqrt(");
+                break;
+            case CBRT:
+                stringBuilder.append("cbrt(");
+                break;
+        }
+
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public String onTrigonometricFunctionButtonsPressed(TrigonometricFunctionTypes type) {
+        switch (type) {
+            case COSINE:
+                stringBuilder.append("cos(");
+                break;
+            case SINE:
+                stringBuilder.append("sin(");
+                break;
+            case TANGENT:
+                stringBuilder.append("tan(");
                 break;
         }
 
@@ -101,6 +193,7 @@ public class CalculatorImplementation implements Calculator {
             return exception.toString();
         }
 
+        stringBuilder.append(result);
         return String.valueOf(result);
     }
 
