e5eecdc568043a4743225cc542e03c7d8807c57e
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-04-25 23:20:22 +0300

    Consider the cases of pressing new buttons

--- app/src/main/java/com/example/calculator/presenter/CalculatorPresenter.java
+++ app/src/main/java/com/example/calculator/presenter/CalculatorPresenter.java
@@ -1,8 +1,13 @@
 package com.example.calculator.presenter;
 
 import com.example.calculator.domain.Calculator;
+import com.example.calculator.enums.BracketButtonTypes;
 import com.example.calculator.enums.DigitButtonTypes;
+import com.example.calculator.enums.IrrationalNumberTypes;
+import com.example.calculator.enums.LogarithmicFunctionTypes;
 import com.example.calculator.enums.OperationButtonTypes;
+import com.example.calculator.enums.RootTypes;
+import com.example.calculator.enums.TrigonometricFunctionTypes;
 import com.example.calculator.ui.CalculatorView;
 
 public class CalculatorPresenter {
@@ -24,6 +29,30 @@ public class CalculatorPresenter {
 
     public void onOperationButtonsPressed(OperationButtonTypes type) {
         calculatorView.display(calculator.onOperationButtonsPressed(type));
+    }
+
+    public void onBracketButtonsPressed(BracketButtonTypes type) {
+        calculatorView.display(calculator.onBracketButtonsPressed(type));
+    }
+
+    public void onDeleteButtonPressed() {
+        calculatorView.display(calculator.onDeleteButtonPressed());
+    }
+
+    public void onTrigonometricFunctionButtonsPressed(TrigonometricFunctionTypes type) {
+        calculatorView.display(calculator.onTrigonometricFunctionButtonsPressed(type));
+    }
+
+    public void onLogarithmicFunctionButtonsPressed(LogarithmicFunctionTypes type) {
+        calculatorView.display(calculator.onLogarithmicFunctionButtonsPressed(type));
+    }
+
+    public void onRootButtonsPressed(RootTypes type) {
+        calculatorView.display(calculator.onRootButtonsPressed(type));
+    }
+
+    public void onIrrationalNumberButtonsPressed(IrrationalNumberTypes type) {
+        calculatorView.display(calculator.onIrrationalNumberButtonsPressed(type));
     }
 
     public void onEqualButtonPressed() {
